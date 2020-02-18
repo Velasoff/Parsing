3ddbe485091f490cfb23b27b2b37780067aa36e6
Velasoff <ivanpelin1508@yandex.ru>
2020-02-18 21:22:32 +0300

    Почистил код

--- Lesson_6/gderuparser/spiders/gderu.py
+++ Lesson_6/gderuparser/spiders/gderu.py
@@ -13,9 +13,12 @@ class GderuSpider(scrapy.Spider):
         self.start_urls = [f'https://gde.ru/yelektronnaya_tehnika?Filter[district_id]=&Filter[search_string]={mark}']
 
     def parse(self, response: HtmlResponse):
+        next_page = response.xpath('//li[@class="text control forward last-none"]/a/@href').extract_first()
         ads_links = response.xpath('//li[@class=""]//div[@class="title"]/a/@href').extract()
         for link in ads_links:
             yield response.follow(link, callback=self.parse_ads)
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
 
     def parse_ads(self, response: HtmlResponse):
         loader = ItemLoader(item=GderuparserItem(), response=response)
@@ -23,10 +26,5 @@ class GderuSpider(scrapy.Spider):
         loader.add_xpath('price', '//div[@class="control-holder"]//span[@class="price"]/text()')
         loader.add_xpath('photos', '//li[@class="slide-item"]//img/@src')
         yield loader.load_item()
-        # name = response.css('h1.title-info-title span.title-info-title-text::text').extract_first()
-#         # photos = response.xpath('//div[contains(@class, "gallery-img-wrapper")]//div[contains(@class, "gallery-img-frame")]/@data-url').extract()
-#         # price = response.xpath('//span[@class="js-item-price"][1]/text()').extract_first()
-#         # yield GderuparserItem(name=name, photos = photos, price = price)
-#         # print(name, photos, price)
 
 
