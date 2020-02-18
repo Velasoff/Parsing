c09e3acbd397a4dad1e06efd316afa53e9057894
Velasoff <ivanpelin1508@yandex.ru>
2020-02-18 21:11:43 +0300

    Lesson_6

--- Lesson_6/gderuparser/runner.py
+++ Lesson_6/gderuparser/runner.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     crawler_settings = Settings()
     crawler_settings.setmodule(settings)
     process = CrawlerProcess(settings=crawler_settings)
-    process.crawl(GderuSpider, mark='asus')
+    process.crawl(GderuSpider, mark='USB')
     process.start()
